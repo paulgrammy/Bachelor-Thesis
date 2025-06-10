f7e73c69ac4804319d59187ce32dc6de79c6df34
paulgrammy <145540009+paulgrammy@users.noreply.github.com>
2025-06-10 15:16:09 +0200

    Commits for LUT

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -7,43 +7,60 @@
 #include "switch_handler.h"
 #include "driver/uart.h"
 
-static const char   *TAG_MAIN = "MAIN";
+static const char *TAG_MAIN = "MAIN";
 
-void setup() {
+void setup()
+{
+    // Disable UART0 to free GPIO1 before setting up the bypass task, required for button 
+    ESP_LOGI(TAG_MAIN, "Disabling UART...");
+    uart_driver_delete(UART_NUM_0);
+    gpio_reset_pin(GPIO_NUM_1);
+
     ESP_LOGI(TAG_MAIN, "Starting setup...");
 
-    // Setup ADC 
+    // Setup ADC
     ESP_LOGI(TAG_MAIN, "Setting up ADC...");
-    if (setup_adc() == false) {
+    if (setup_adc() == false)
+    {
         ESP_LOGE(TAG_MAIN, "Failed to setup ADC!");
-    } else {
+    }
+    else
+    {
         // ID the board based on voltage
         ESP_LOGI(TAG_MAIN, "Identified board: %s", get_board_id());
     }
 
     // Setup Button GPIO task
     ESP_LOGI(TAG_MAIN, "Setting up GPIO task...");
-    if(setup_gpio_task()){
+    if (setup_gpio_task())
+    {
         ESP_LOGI(TAG_MAIN, "GPIO task created successfully.");
-    } else {
+    }
+    else
+    {
         ESP_LOGE(TAG_MAIN, "Failed to create GPIO task!");
+    }
+
+    // Setup Bypass GPIO task
+    ESP_LOGI(TAG_MAIN, "Setting up Bypass task...");
+    if (setup_bypass_task())
+    {
+        ESP_LOGI(TAG_MAIN, "Bypass task created successfully.");
+    }
+    else
+    {
+        ESP_LOGE(TAG_MAIN, "Failed to create Bypass task!");
     }
 
     ESP_LOGI(TAG_MAIN, "Setup succesfully completed.");
 
-    // Disable UART0 to free GPIO1
-    uart_driver_delete(UART_NUM_0);
-    gpio_reset_pin(GPIO_NUM_1);
+    // Blink LED to indicate setup is complete
+    // Setup LED GPIO
+    gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT);           // Setup Internal LED
 
-    // // Setup Bypass GPIO task
-    // ESP_LOGI(TAG_MAIN, "Setting up Bypass task...");
-    // if(setup_bypass_task()){
-    //     ESP_LOGI(TAG_MAIN, "Bypass task created successfully.");
-    // } else {
-    //     ESP_LOGE(TAG_MAIN, "Failed to create Bypass task!");
-    // }
+    gpio_set_level(GPIO_NUM_5, 1);                              // Turn LED on
+    vTaskDelay(pdMS_TO_TICKS(500));                             // Delay to allow LED to turn on
+    gpio_set_level(GPIO_NUM_5, 0);                              // Turn LED off
 }
 
 void loop() {}
-
-
--- software/src/switch_handler.h
+++ software/src/switch_handler.h
@@ -1,24 +1,33 @@
-// #ifndef SWITCH_HANDLER_H
-// #define SWITCH_HANDLER_H
+#ifndef SWITCH_HANDLER_H
+#define SWITCH_HANDLER_H
 
-// #include <Arduino.h>
-// #include <esp_log.h>
-// #include <freertos/FreeRTOS.h>
-// #include <freertos/task.h>
-// #include <driver/gpio.h>
+#include <Arduino.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
 
-// // declarations
-// #define GPIO_STACK_SIZE 4096 // Stack size for the task
-// #define TASK_PRIORITY_SWITCH 7    // Task priority
-// #define BYPASS_PIN 1 // GPIO pin for the button
-// #define HI_LO_PIN 26 // GPIO pin for the high/low signal output pin
+// declarations
+#define GPIO_STACK_SIZE 4096 // Stack size for the task
+#define TASK_PRIORITY_SWITCH 3    // Task priority
+#define BYPASS_PIN 1 // GPIO pin for the button
+#define HI_LO_PIN 26 // GPIO pin for the high/low signal output pin
 
-// // public
+// public
 
-// bool setup_bypass_task(void);
+/* * @brief Setup the bypass task
+ * This function sets up the bypass task and initializes the GPIO pins.
+ * @return true if the task was created successfully, false otherwise
+ */
+bool setup_bypass_task(void);
 
-// // private 
+// private 
 
-// void bypass_event_loop(void *pvParameters);
+/* * @brief Bypass event loop task
+ * This task handles the bypass button press events and toggles the hi_lo pin state.
+ * On button press, a logic level change is sent to the hi_lo pin, switching the MUX.
+ * @param pvParameters Pointer to task parameters
+ */ 
+void bypass_event_loop(void *pvParameters);
 
-// #endif // SWITCH_HANDLER_H
+#endif // SWITCH_HANDLER_H
